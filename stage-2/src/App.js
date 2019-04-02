bc7c0afb2b17d58637250faa669b8752ad04dbd9
Bryan Hawkins <bhawkins3729@gmail.com>
2019-04-02 16:23:04 -0500

    finished stage 2

--- stage-2/src/App.js
+++ stage-2/src/App.js
@@ -21,17 +21,17 @@ class App extends Component {
       .get("https://practiceapi.devmountain.com/products/")
       .then(response => {
         this.setState({
-          products: response
+          products: response.data
         });
       });
   }
   addToCart(item) {
     this.setState({
-      cart: item
+      cart: [...this.state.cart, item]
     });
   }
   removeFromCart(index) {
-    let cartCopy = this.state.products.slice();
+    let cartCopy = this.state.cart.slice();
     cartCopy.splice(index, 1);
     this.setState({
       cart: cartCopy
